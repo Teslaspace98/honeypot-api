a51393bb6147dd58a2b0d86ac4ea220a4dbc6139
Teslaspace98 <spacetesla6@gmail.com>
2026-02-04 19:46:02 +0530

    Change honeypot function to async and add Request param

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-from fastapi import FastAPI, Header, HTTPException, Body
+from fastapi import FastAPI, Header, HTTPException, Request
 
 app = FastAPI()
 
 API_KEY = "test123"
 
 @app.api_route("/honeypot", methods=["POST", "GET"])
-def honeypot(
-    payload: dict = Body(default=None),
+async def honeypot(
+    request: Request,
     x_api_key: str = Header(None)
 ):
     if x_api_key != API_KEY:
         raise HTTPException(status_code=401, detail="Invalid API key")
 
+    # DO NOT parse body at all (tester sends weird payloads)
     return {
         "scam_detected": False,
         "confidence": 0.5,
